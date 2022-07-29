5329b5475884b0c8e7dd98228181bf60f229ff32
DiFortis <difortis3@gmail.com>
2022-07-29 13:08:04 +0200

    Completed Task 3 - Trophies View

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class _RandomWordsState extends State<RandomWords> {
       MaterialPageRoute<void>(
         builder: (context) {
           final tiles = _saved.map(
-            (pair) {
+                (pair) {
               return ListTile(
                 title: Text(
                   pair.asPascalCase,
@@ -41,9 +41,9 @@ class _RandomWordsState extends State<RandomWords> {
           );
           final divided = tiles.isNotEmpty
               ? ListTile.divideTiles(
-                  context: context,
-                  tiles: tiles,
-                ).toList()
+            context: context,
+            tiles: tiles,
+          ).toList()
               : <Widget>[];
 
           return Scaffold(
@@ -197,14 +197,15 @@ class _AwesomeTabs extends State<AwesomeTabs> {
   int _selectedIndex = 0;
   static const List<Widget> _widgetOptions = <Widget>[
     Center(
-        child: Text(
-          'Hello GroMar!',
-          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-        ),
+      child: Text(
+        'Hello GroMar!',
+        style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
       ),
+    ),
     CoolCounter(),
     RandomWords(),
     Notifications(),
+    Trophies(),
   ];
 
   void _onItemTapped(int index) {
@@ -237,6 +238,11 @@ class _AwesomeTabs extends State<AwesomeTabs> {
           BottomNavigationBarItem(
             icon: Icon(Icons.notifications),
             label: 'Task1:Notifications',
+            backgroundColor: Colors.blue,
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications),
+            label: 'Task3:Trophies',
             backgroundColor: Colors.blue,
           ),
         ],
@@ -514,4 +520,152 @@ class Notifications extends StatefulWidget {
 
   @override
   State<Notifications> createState() => _NotificationsState();
+}
+//Trophies
+class Trophies extends StatefulWidget {
+  const Trophies({Key? key }) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+
+  @override
+  State<Trophies> createState() => _TrophiesState();
+
+}
+
+
+class _TrophiesState extends State<Trophies> {
+  int _counterpoints = 0;
+  int _countertrophy = 0;
+  int _countertrophiesleft = 100;
+  String message1='100';
+
+  void _incrementCounterPoints() {
+    setState(() {
+
+      _counterpoints++;
+    });
+  }
+  void _incrementCounterTrophy() {
+    if (_countertrophiesleft > 0) {
+      setState(() {
+        _countertrophy++;
+        _countertrophiesleft--;
+        message1 = _countertrophiesleft.toString();
+      });
+    }
+    if (_countertrophiesleft == 0) {
+      setState(() {
+        message1 = 'There are no trophies left!';
+      }
+      );
+    }
+  }
+  void _addnewtrophiestoearn() {
+    setState(() {
+
+      _countertrophiesleft++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: const Color.fromRGBO(25, 25, 25, 1.0),
+
+          // Here we take the value from the MyHomePage object that was created by
+          // the App.build method, and use it to set our appbar title.
+          title: const Text('Trophies'),
+          titleSpacing: 00.0,
+          centerTitle: true,
+          toolbarHeight: 60.2,
+          toolbarOpacity: 0.8,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(25),
+                topRight: Radius.circular(25),
+                bottomRight: Radius.circular(25),
+                bottomLeft: Radius.circular(25)),
+          ),
+          leading: SizedBox(
+            width: double.infinity,
+            child: IconButton(
+              icon: const FaIcon(FontAwesomeIcons.arrowLeft, color: Color.fromRGBO(218, 165, 32, 1.0)),
+              onPressed: (){/*ToDo*/},
+            ),
+          ),
+
+        ),
+        body:Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+
+              children:  <Widget> [
+                const Padding(padding: EdgeInsets.all(20)),
+                const Text('All Trophies', style: TextStyle(
+                    color: Colors.white, fontSize: 30, fontWeight: FontWeight.bold),),
+                const SizedBox(height: 20,),
+                Expanded(
+                    child: GridView(
+                      shrinkWrap: true,
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 3,
+                      ),
+                      children: [
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            IconButton(onPressed: _incrementCounterTrophy, tooltip: 'Earn a trophy', icon: const Icon(FontAwesomeIcons.trophy, color: Color.fromRGBO(218, 165, 32, 1.0))),
+                            const SizedBox(height: 10),
+                            Text('Trophies earned: $_countertrophy', style: const TextStyle(color: Colors.white)),
+                          ],
+                        ),
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children:  [
+                            IconButton(onPressed: _addnewtrophiestoearn, tooltip: 'Add trophies to earn', icon: const Icon(FontAwesomeIcons.trophy, color: Color.fromRGBO(218, 165, 32, 1.0))),
+                            const SizedBox(height: 10),
+                            Text('Trophies to earn: $message1', style: const TextStyle(color: Colors.white)),
+                          ],
+                        ),
+
+                        Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            //Icon(FontAwesomeIcons.database,color: Color.fromRGBO(218, 165, 32, 1.0)),
+                            IconButton(onPressed: (){
+                              _incrementCounterPoints();
+                            }, tooltip: 'Earn a point', icon: const Icon(FontAwesomeIcons.database, color: Color.fromRGBO(218, 165, 32, 1.0))),
+                            const SizedBox(height: 10),
+                            Text('Points earned: $_counterpoints', style: const TextStyle(color: Colors.white)),
+
+                          ],
+                        ),
+                      ],
+                    )
+                )
+              ],
+            )
+        )
+    );
+  }
 }
