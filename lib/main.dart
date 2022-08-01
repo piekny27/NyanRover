3d0441a01c110a765c32b43871e6dc714156f285
Mateusz Piekny <m.piekny97@gmail.com>
2022-08-01 07:40:39 +0200

    Fixed search; branch cleanup

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:english_words/english_words.dart';
 import 'package:flutter/material.dart';
-import 'notification_widget.dart';
+import 'components/notification_widget.dart';
 
 
 void main() {
